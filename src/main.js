1850f932e909426b590aaaf83e7209bb55a6e3a3
lawrencemboya <lawrencemboya@gmail.com>
2019-07-02 14:09:51 -0400

    MVP

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "./main.css"
+
+import Fireworks from "fireworks-canvas"
+
+export default () => {
+  const container = document.getElementById("container")
+  const fireworks = new Fireworks(container)
+  fireworks.start()
+}
